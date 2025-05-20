24434bdde50f10202bc88177c8e2fe2c8c1fcbc8
ErikTeixeira <erik.barbosa515@gmail.com>
2025-05-19 23:40:14 -0300

    Containerização com Docker

--- src/test/java/br/com/projCarro/CarroProjeto/CarroProjetoApplicationTests.java
+++ src/test/java/br/com/projCarro/CarroProjeto/CarroProjetoApplicationTests.java
@@ -2,8 +2,13 @@ package br.com.projCarro.CarroProjeto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 @SpringBootTest
+@ActiveProfiles("test")                       // usa application-test.properties
+@AutoConfigureTestDatabase(replace = Replace.ANY) // instrui o Spring a usar o banco de teste (H2)
 class CarroProjetoApplicationTests {
 
 	@Test
